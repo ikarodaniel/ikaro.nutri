7bdb839769aa68dc3957036048f1ac1ef3fb9ba6
ikarodaniel <ikaro.vanderlei@escola.pr.gov.br>
2025-08-27 09:33:26 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,20 @@
+var pacientes = document.querySelector(".titulo-secundario");
+pacientes.textContent ="meus pacientes";
+ var pacientes = document.querySelector(".titulo");
+pacientes.textContent = "ikaro nutri";
+
+var  paciente = document.querySelector("#primeiro-paciente");
+var tdpeso = pacientes = document.querySelectior(".titulo-secundario");
+pacientes.textcontent ="meus pacientes";
+
+var tdaltura = paciente.querySelector(".info-altura");
+var altura = tdaltura.textContent
+
+
+
+console.log(tdpeso)
+console.log(peso);
+console.log(tdaltura);
+console.log(altura);
+
+
